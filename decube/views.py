4d1838924eef37b0f4e60a5ce65ccc0450b74f0a
Kota Nakasaka <web.knakasaka@gmail.com>
2022-06-27 14:24:30 +0900

    main

---
+++ decube/views.py
@@ -0,0 +1,30 @@
+from re import template
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.conf import settings
+from django.core.mail import BadHeaderError, EmailMessage, send_mail
+from django.http import HttpResponse
+import textwrap
+import urllib.parse
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        url = request.build_absolute_uri()
+        qs = urllib.parse.urlparse(url).query
+
+        subject = "【あしあと】配布したQRコードから貴社サイトにアクセスがありました"
+        message = qs
+        from_email = "decube.ashiato@gmail.com"
+        recipient_list = ["web.decube@gmail.com"]
+        try:
+            send_mail(subject, message, from_email, recipient_list)
+        except BadHeaderError:
+            return HttpResponse('無効なヘッダが検出されました。')
+        return render(request, 'decube/redirect.html')
+    template_name = 'index.html'
+
+class RedirectView(View):
+    template_name = 'redirect.html'
+    
---
+++ webchallenge/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebchallengeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'webchallenge'
